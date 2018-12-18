18119f55a3a1b2f047073f3375cc2722daac446d
Yibin Wei <yibinw.92@gmail.com>
2018-12-18 17:36:18 -0500

    first commit

---
+++ public/assets/js/main.js
@@ -0,0 +1,112 @@
+$(function () {
+    var searchText = $("#searchText");
+
+
+    $('#logout').on('click', () => {
+
+        $.get("/logout", () => {
+            window.location.href = '/';
+        });
+    })
+
+    $(document).on("submit", "#search-form", handleSearchFormSubmit);
+
+    // A function to handle what happens when the form is submitted to create a new Author
+    function handleSearchFormSubmit(event) {
+        event.preventDefault();
+        // Don't do anything if the name fields hasn't been filled out
+
+        if (!searchText.val().trim().trim()) {
+            return;
+        }
+        // Calling the upsertAuthor function and passing in the value of the name input
+        searchCriteria({
+            search: searchText
+                .val()
+                .trim()
+        });
+    }
+
+    var dataResponse = [];
+
+    function searchCriteria(searchData) {
+        console.log()
+        $("#dataLogin").empty();
+
+        $.get("/search/" + searchData.search, function(data) {
+            dataResponse = JSON.parse(data).results;
+
+            console.log(dataResponse)
+
+            for(let i = 0; i < 10; i++){
+                
+                var rowData = $("<tr>");
+                rowData.addClass("product-data");
+                rowData.attr("id", i);
+
+                var colName = $("<td>");
+                colName.append(dataResponse[i].name);
+
+                var colCategory = $("<td>");
+                colCategory.append(dataResponse[i].category);
+
+                var colBrand = $("<td>");
+                colBrand.append(dataResponse[i].brand);
+
+                var colPrice = $("<td>");
+                colPrice.append(dataResponse[i].price);
+
+                
+
+                var colQuantity = $("<td>");
+                colQuantity.append("<input id='quantity' type='text' class='validate' style='width: 50px; text-align: center;'></input>");
+
+                var colAdd = $("<td id='add'>");
+                colAdd.append("<i class='material-icons'>add</i>");
+
+                rowData.append(colName)
+                    .append(colCategory)
+                    .append(colBrand)
+                    .append(colPrice)
+                    .append(colQuantity)                    
+                    .append(colAdd)
+
+                $("#dataProducts").append(rowData);
+            }            
+        })
+    }
+
+    $(document).on("click", "#add", function () {
+
+        $("#dataLogin").empty();
+        
+        toEdit = $(this).parent().attr("id");
+        console.log(toEdit)
+
+        var chosenEntry = dataResponse[toEdit];
+        console.log(chosenEntry);
+
+        var chosenQuantity = $(this).closest('tr').find('input').val();
+
+        console.log(chosenQuantity);
+
+        // var docum = db.collection("trains").doc($(this).parent().attr("id")).get().then(function (doc) {
+        //     $("#inputName").val(doc.data().name);
+        //     $("#inputDestination").val(doc.data().destination);
+        //     $("#inputStart").val(doc.data().startTime);
+        //     $("#inputFrequency").val(doc.data().frequency);
+        //     $("#newTrainSubmit").text("Update Train");
+        // })
+
+        var newProduct = {
+            name: chosenEntry.name,
+            category: chosenEntry.category,
+            price: chosenEntry.price,
+            manufacturer: chosenEntry.manufacturer || chosenEntry.brand,
+            quantity: chosenQuantity,
+        };
+
+        console.log(newProduct);
+    });
+
+})
